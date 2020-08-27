546c671513fbe0b90992785997c3f82dadf118bb
ema.skottova <ema.skottova@mygymer.ch>
2020-08-27 19:44:36 +0200

    Input Generator First Line printing
    input_generator.cpp now prints the first line of the input (number of events in total, number of events per type, capacities, and number of time steps until a line change is allowed). It can now be used without issues.

--- input_generator.cpp
+++ input_generator.cpp
@@ -11,7 +11,7 @@ class Input
         const int max_coordinate_d = 100;
         int cur_station_types = 0;
         // station, passenger, line, train
-        const vector<int> event_p = {120, 3890, 11, 33}; // probabilities for each event (taken from input file)
+        const vector<int> event_p = {120, 3890, 20, 60}; // probabilities for each event ({120, 3890, 11, 33} in input file)
         vector<int> event_n = {0, 0, 0, 0}; // number of times an event has appeared
         vector<int> event_c = {0, 0, 0, 0, 0}; //cutoffs for the events
         vector<pair<int, int>> station_coordinates;
@@ -23,6 +23,30 @@ class Input
                 event_c[i] = event_c[i - 1] + event_p[i - 1];
             }
         }
+
+        class Event
+        {
+            private:
+                int time;
+                char type;
+                int id;
+                vector<int> other;
+            public:
+                Event(int time, char type, int id, vector<int>& other)
+                {
+                    this->time = time;
+                    this->type = type;
+                    this->id = id;
+                    this->other = other;
+                }
+                void print()
+                {
+                    cout << this->time << " " << this->type << " " << this->id << " ";
+                    for(int el : other) cout << el << " ";
+                    cout << '\n';
+                }
+        };
+
     public:
         Input(int n, int seed)
         {
@@ -32,16 +56,22 @@ class Input
             mt19937 rng(seed);
             uniform_int_distribution<int> uni(0, 1e12);
 
+            vector<Event> events;
             int t = 0; // time since the beginning 
             for(int i = 0; i < n; i ++)
             {
                 int e_type = uni(rng) % event_c[4]; // random event type
                 if(i < 3) e_type = 0;
+                else if(i == 3) e_type = event_c[3];
+                else if(i == 4) e_type = event_c[4];
                 int j = 0;
                 while(e_type > event_c[j + 1]) j ++; // find type
 
                 int index = event_n[j];
                 event_n[j] ++;
+
+                vector<int> other;
+                char typec;
                 
                 switch (j) // generates random input for the type and prints it
                 {
@@ -72,36 +102,47 @@ class Input
                         }
                         station_set.insert({x, y});
                         station_coordinates.push_back({x, y});
-                        cout << t << " s " << index << " " << type << " " << x << " " << y << '\n';
+                        other = {x, y, type};
+                        typec = 's';
                         break;
                     }
                     case 1:
                     {
                         int dep_station = uni(rng) % station_coordinates.size(); // makes sure passenger starts at a valid station
                         int arr_type = uni(rng) % cur_station_types; // ensures that the station type is already active
-                        cout << t << " p " << index << " " << dep_station << " " << arr_type << '\n';
+                        other = {dep_station, arr_type};
+                        typec = 'p';
                         break;
                     }
                     case 2:
                     {
-                        cout << t << " l " << index << '\n';
+                        other = {};
+                        typec = 'l';
                         break;
                     }
                     default:
                     {
-                        cout << t << " z " << index << '\n';
+                        other = {};
+                        typec = 'z';
                         break;
                     }
                 }
-                t += uni(rng) % 50;
+                events.push_back(*new Event(t, typec, index, other));
+                t += uni(rng) % 20;
             }
+
+            int change_time = uni(rng) % 200 + 20, train_passengers = uni(rng) % 30 + 5, station_passengers = uni(rng) % 60 + 20;
+
+            cout << n << " " << change_time << " " << train_passengers << " " << station_passengers << " " << event_n[0] << " " << event_n[1] << " " << event_n[2] << " " << event_n[3] << '\n';
+            
+            for(Event e : events) e.print();
         }
 };
 
 signed main()
 {
-    int n, seed;
-    cin >> n >> seed;
+    int n = 2000, seed = 10007;
+    //cin >> n >> seed;
     Input in(n, seed);
     return 0;
 }
